2737ea993fe87ea2f0ad9483d6dbb3063dc0ed7e
Wojciech Pituła <wojciech@swissborg.com>
2024-10-26 09:14:39 +0200

    Init

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,97 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+
+const config: Config = {
+    title: 'Business4s',
+    tagline: 'Scala Community Focused On Business Problems',
+    favicon: 'img/favicon.ico',
+
+    // GitHub pages deployment config.
+    url: 'https://business4s.github.io/',
+    baseUrl: '/',
+    organizationName: 'business4s',
+    projectName: 'business4s.github.io',
+    trailingSlash: true,
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: false,
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        image: 'img/Business4s.drawio.png',
+        navbar: {
+            title: 'Business4s',
+            logo: {
+                alt: 'Business4s Logo',
+                src: 'img/Business4s.drawio.png',
+            },
+            items: [
+                {
+                    href: 'https://medium.com/business4s-blog',
+                    label: 'Blog',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/business4s/decisions4s',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+                {
+                    href: 'https://bit.ly/business4s-discord',
+                    label: 'Discord',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                  title: 'Community',
+                  items: [
+                    {
+                      label: 'Discord',
+                      href: 'https://bit.ly/business4s-discord',
+                    },
+                    {
+                      label: 'Twitter',
+                      href: 'https://twitter.com/business4scala',
+                    },
+                  ],
+                },
+            ],
+            // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+            additionalLanguages: ['java', 'scala', "json"]
+        },
+    } satisfies Preset.ThemeConfig,
+    customFields: {
+        decisions4sVersion: process.env.DECISIONS4S_VERSION,
+    },
+};
+
+export default config;
---
+++ website/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,74 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+    title: string;
+    Svg?: React.ComponentType<React.ComponentProps<'svg'>>;
+    description: JSX.Element;
+    link: string;
+};
+
+const FeatureList: FeatureItem[] = [
+    {
+        title: 'Decisions4s',
+        Svg: require('@site/static/img/decisions4s-logo.drawio.svg').default,
+        description: (
+            <>
+                A library helping with taming the complexity of conditional logic.
+            </>
+        ),
+        link: 'https://business4s.github.io/decisions4s/'
+    },
+    {
+        title: 'Domain-Oriented Scala Style Guide',
+        Svg: require('@site/static/img/domain-scala-guide.drawio.svg').default,
+        description: (
+            <>
+                An opinionated Scala style guide helping to focus the code on the business domain instead of technical
+                aspects.
+            </>
+        ),
+        link: 'https://github.com/business4s/domain-oriented-scala-style-guide'
+    },
+    {
+        title: 'Workflows4s',
+        Svg: require('@site/static/img/workflows4s-logo.drawio.svg').default,
+        description: (
+            <>
+                Prototype of a library for building long-running stateful workflows.
+            </>
+        ),
+        link: 'https://business4s.github.io/workflows4s/'
+    },
+];
+
+function Feature({title, Svg, description, link}: FeatureItem) {
+    return (
+        <div className={clsx('col col--4')}>
+            <a href={link} target="_blank" rel="noopener noreferrer" className="feature-link">
+                <div className="text--center padding-horiz--md">
+                    {Svg && <div className="text--center">
+                        <Svg className={styles.featureSvg} role="img"/>
+                    </div>}
+                    <Heading as="h3">{title}</Heading>
+                    <p>{description}</p>
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
